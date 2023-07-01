53f72aa136e99b1e0e0b61134af164adb369e381
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2023-07-01 02:16:58 -0300

    alteração JS

--- src/js/gerenciar-despesas.js
+++ src/js/gerenciar-despesas.js
@@ -1,43 +1,101 @@
-// Obtém o formulário de adicionar despesas
-var formularioDespesas = document.getElementById('meuFormulario');
+const despesas = JSON.parse(window.localStorage.getItem('despesas')) || [];
+const usuario = JSON.parse(localStorage.getItem('userLogado')) || [];
 
-// Adiciona um ouvinte de evento para o envio do formulário
-formularioDespesas.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita o envio do formulário
+function inserirDespesa() {
+    let counter = despesas.length; // div com a despesa
+    let id = counter + 1;
+    let nome = document.getElementById('nome').value;
+    let vlTotal = document.getElementById('vlTotal').value;
 
-  // Obtém os valores inseridos pelo usuário
-  var nomeDespesa = document.getElementById('nomeDespesa').value;
-  var valorDespesa = document.getElementById('valorDespesa').value;
-  var categoriaDespesa = document.getElementById('categoriaDespesa').value;
-
-  // Obtém os elementos das despesas existentes
-  var despesa1 = document.getElementById('despesa1');
-  var despesa2 = document.getElementById('despesa2');
-  var despesa3 = document.getElementById('despesa3');
-
-  // Atualiza os elementos das despesas existentes com os valores inseridos pelo usuário
-  document.getElementById('nomeDespesa1').innerText = 'Nome da despesa adicionada: ' + nomeDespesa;
-  document.getElementById('valorDespesa1').innerText = 'Valor: R$' + valorDespesa;
-  document.getElementById('categoriaDespesa1').innerText = 'Categoria: ' + categoriaDespesa;
-
-  document.getElementById('nomeDespesa2').innerText = 'Nome da despesa adicionada: ' + nomeDespesa;
-  document.getElementById('valorDespesa2').innerText = 'Valor: R$' + valorDespesa;
-  document.getElementById('categoriaDespesa2').innerText = 'Categoria: ' + categoriaDespesa;
-
-  document.getElementById('nomeDespesa3').innerText = 'Nome da despesa adicionada: ' + nomeDespesa;
-  document.getElementById('valorDespesa3').innerText = 'Valor: R$' + valorDespesa;
-  document.getElementById('categoriaDespesa3').innerText = 'Categoria: ' + categoriaDespesa;
-});
-
-function restaurarFormaPadrao(idDespesa) {
-    const nomeDespesa = document.getElementById(`nome${idDespesa}`);
-    const valorDespesa = document.getElementById(`valor${idDespesa}`);
-    const categoriaDespesa = document.getElementById(`categoria${idDespesa}`);
-  
-    if (nomeDespesa && valorDespesa && categoriaDespesa) {
-      nomeDespesa.innerText = "Nome da despesa adicionada:";
-      valorDespesa.innerText = "Valor: R$____,__";
-      categoriaDespesa.innerText = "Categoria:";
+    let despesa = {
+        id: id,
+        userEmail: usuario.email,
+        nome: nome,
+        vlTotal: vlTotal,
     }
-  }
+
+    despesas.push(despesa);
+    window.localStorage.setItem("despesas", JSON.stringify(despesas));
+    exibirDespesas();
+}
+
+function exibirDespesas() {
+    let listaDespesas = document.getElementById('despesas-criadas'); // div que vai receber as despesas criadas
+    if (despesas.length > 0) {
+        despesas.forEach(despesa => {
+            if (usuario.email == despesa.userEmail) {
+                const newDiv = document.createElement('div');
+                newDiv.classList.add('minhas-despesas');
+                const newBR = document.createElement('br');
+    
+                const total = parseFloat(despesa.vlTotal).toLocaleString("pt-BR", 
+                { style: "currency" , currency:"BRL"});
+    
+                const elementoNome = document.createElement('h4');
+                const elementoVlTotal = document.createElement('p');
+    
+                const nome = document.createTextNode(despesa.nome);
+                const vlTotal = document.createTextNode(`Total: ${total}`);
+                
+                elementoNome.appendChild(nome);
+                elementoNome.innerHTML += "<span class=\"material-symbols-outlined\" data-bs-toggle=\"modal\" data-bs-target=\"#editDespesaModal\" onclick=\"preparaFormAtualizacao("+despesa.id+")\">edit</span>";
+                elementoNome.innerHTML += "<span class=\"material-symbols-outlined\" onclick=\"excluirDespesa("+despesa.id+")\">delete</span>";
+                elementoVlTotal.appendChild(vlTotal);
+              
+                newDiv.appendChild(elementoNome);
+                newDiv.appendChild(newBR);
+                newDiv.appendChild(elementoVlTotal);
+               
+                listaDespesas.appendChild(newDiv);
+            }
+        });
+    }
+}
+
+function preparaFormAtualizacao(id) {
+    // Para os campos de atualização
+    const editNome = document.getElementById('editNome');
+    const editVlTotal = document.getElementById('editVlTotal');
+    const despesaId = document.getElementById('despesaId');
+
+    despesas.forEach(despesa => {
+        if (despesa.id == id) {
+            editNome.value = despesa.nome;
+            editVlTotal.value = despesa.vlTotal;
+            despesaId.value = id;
+        }
+    });
+}
+
+function atualizarDespesa() {
+  const id = document.getElementById('despesaId').value;
+  const editNome = document.getElementById('editNome').value;
+  const editVlTotal = document.getElementById('editVlTotal').value;
   
+  despesas.forEach(despesa => {
+      if (despesa.id == id) {
+          despesa.nome = editNome;
+          despesa.vlTotal = editVlTotal;
+      }
+  });
+  
+  window.localStorage.setItem("despesas", JSON.stringify(despesas));
+  window.location.reload();
+}
+
+function excluirDespesa(id) {
+    despesas.forEach((despesa, index) => {
+        if (despesa.id == id) {
+            despesas.splice(index, 1);
+            localStorage.removeItem('despesas');
+            window.localStorage.setItem("despesas", JSON.stringify(despesas));
+            window.location.reload();
+        }
+    });
+}
+
+window.onload = function(){ //ensures the page is loaded before functions are executed.
+    exibirDespesas();
+}
+
+
